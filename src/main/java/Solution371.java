7b8f56f767057a622eda35a12d10a19c6962d877
raymondyan <raymond.dream@gmail.com>
2021-01-19 16:40:27 +0800

    [#371] sum of two integers

---
+++ src/main/java/Solution371.java
@@ -0,0 +1,8 @@
+public class Solution371 {
+  /*
+    不使用加减法算和
+   */
+  public int getSum(int a, int b) {
+    return Integer.sum(a,b);
+  }
+}
---
+++ src/test/java/Solution371Test.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Solution371Test {
+
+  @Test
+  void name() {
+    Solution371 solution371 = new Solution371();
+    int res = solution371.getSum(1, 2);
+    assertEquals(3, res);
+  }
+}
